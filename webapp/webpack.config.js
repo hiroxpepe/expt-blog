8dd5e75a0ff92aca7d52e5705cdb83b0a61e18cc
Hiroyuki Adachi <hiroxpepe@gmail.com>
2020-06-06 15:18:08 +0900

    moved source javascript files.

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   mode: 'development',
-  entry: './src/main/assets/scripts/main.js',
+  entry: './src/main/resources/assets/main.js',
   output: {
     filename: 'exmp-blog.min.js',
     path: path.join(__dirname, 'src/main/webapp/docroot/scripts')
